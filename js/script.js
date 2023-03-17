a408a8c25769972572c4e767652836af21a25149
StefanoM98 <smartino030@gmail.com>
2023-03-17 17:07:36 +0100

    Add: script done

---
+++ js/script.js
@@ -0,0 +1,35 @@
+// INIZIAMO RICHIAMANDO I NUMERI CHE CI SERVONO DA STAMPARE
+const easyBox = 100;
+const mediumBox = 81;
+const hardBox = 49;
+
+// ORA GENERIAMO LE CASELLE IN BASE AL NUMERO 
+const gridElem = document.querySelector(".grid")
+const btnStart = document.querySelector("button")
+
+btnStart.addEventListener("click", getEasyBox)
+
+// for (let i = 0; i < numbers; i++) {
+//     const currentNumber = number[i];
+//     const newItem = generateNewBox[currentNumber];
+//     gridElem.append(newItem)
+// }
+
+// FUNZIONI
+
+// GENERIAMO I NUMERI PER CASELLA
+function getEasyBox () {
+    for (let i =1; i <= 100; i++) {
+        const grid = generateNewBox(i)
+        gridElem.append(grid)
+    }
+}
+
+// CREIAMO LA FUNZIONE PER IL BOX
+function generateNewBox(testo) {
+    const newBox = document.createElement("div");
+    newBox.classList.add ("grid-item");
+    newBox.classList.add ("easy");
+    newBox.innerHTML = `${testo}`;
+    return newBox;
+}
